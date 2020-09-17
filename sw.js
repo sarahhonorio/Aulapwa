babdabbc56f43307b6927182c9f3e02381e98e94
sarahhonorio <sarah.martins01@etec.sp.gov.br>
2020-09-17 14:30:05 -0300

    Commit - aprimorando

--- sw.js
+++ sw.js
@@ -1,3 +1,22 @@
+var CACHE_NAME = 'sarahhonorio-cache-v1'
+var urlsToCache = [
+    'css/sarahhonorio.css',
+    'css/bootstrap.css',
+    'js/bootstrap.js',
+    'js/jquery-3.5.1.js',
+    'libs/sweetalert2/src/sweetalert2.js'
+
+]
+self.addEventListener('install', function(event){
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+        .then(function(cache){
+            console.log('Cache aberto...')
+            return cache.addAll(urlsToCache)
+        })
+    )
+})
+
 self.addEventListener('fetch', function(event){
-    
+
 })
