4c68fdcdcba5cf1f3c5daaa0ecd938d0f41e8fe6
sarahhonorio <sarah.martins01@etec.sp.gov.br>
2020-10-08 15:55:00 -0300

    Commit - sw

--- sw.js
+++ sw.js
@@ -24,7 +24,35 @@ self.addEventListener('fetch', function(event){
             if(response){
                 return responde
             }
-            return fetch(event.request)
+            var fetchRequest = event.request.clone ()
+            return fetch(fetchRequest).then(
+                function(response){
+                    if(!response || response.status !== 200 || response.type !== 'basic'){
+                        return response
+                    }
+                    var respondeToCache = resposta.clone()
+                    caches.open(CACHE_NAME).then(
+                        function(cache){
+                            cache.put(event.request, responseToCache)
+                        }
+                    )
+                    return response
+                }
+            )
+        })
+    )
+})
+self.addEventListener('active', function(event){
+    var cacheAllowlist = ['blog-cache-v1', 'blog-cache-v2']
+    event.waitUntil(
+        caches.keys().then(function(cacheNames){
+            return Promisse.all(
+                cacheNames.map(function(cacheName){
+                    if(cacheAllowlist.indexOf(cacheName) == -1){
+                        return cache.delete(cacheName)
+                    }
+                })
+            )
         })
     )
 })
